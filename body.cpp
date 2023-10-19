552e4918fb37538af15c6d39d18692392ccd041f
Leo-Lepin <129879137+Leo-Lepin@users.noreply.github.com>
2023-10-19 16:48:47 +0300

    Update body.cpp

--- body.cpp
+++ body.cpp
@@ -146,82 +146,67 @@ bool win(char** tablepl1ship, char** tablepl2ship) {
 }
 
 bool isShipExist(char** table) {
-	if ((xl > 0 && table[xl][y] != 'k') && (xr < 10 && table[xr][y] != 'k') &&
-		(yu > 0 && table[x][yu] != 'k') && (yd < 10 && table[x][yd] != 'k'))
-		return 0;
-	return 1;
+	if ((xl > 0 && table[y][xl-1] == 'k') || (xr < 10 && table[y][xr+1] == 'k') ||
+		(yu > 0 && table[yu-1][x] == 'k') || (yd < 10 && table[yd+1][x] == 'k'))
+		return 1;
+	return 0;
 }
 
 void aiIfShot(char** tableBot, char** tablePl) {
-	int xn = -1, yn = -1;
-	if ((l || r || u || d) == 0) {
-		l = 1; r = 1; d = 1; u = 1;
-		yd = yu = y;
-		xr = xl = x;
+	if ((l || r || d || u) == 0) {
+		l = 1; r = 1; u = 1; d = 1;
+		yu = y; yd = y; xr = x; xl = x;
 	}
-	int rnd = rand() % 2;
-
-	if (rnd && (yu - 1 > 0 || yd + 1 < 10) || (l + r == 0)) {
-		rnd = rand() % 2;
-		if (rnd && u && yu - 1 >= 0) {
-			xn = x; yn = yu-1;
-			if (tablePl[yu-1][x] == 'k')
-			{
-
+	vector <pair <int, int>> possible;
+	if (l && xl - 1 >= 0)
+		possible.push_back({ y, xl - 1 });
+	if (r && xr + 1 < 10)
+		possible.push_back({ y, xr + 1 });
+	if (u && yu - 1 >= 0)
+		possible.push_back({ yu - 1, x });
+	if (d && yd + 1 < 10)
+		possible.push_back({ yd + 1, x });
+	if (possible.size() == 0) {
+		cout << "ERROR\n";
+		return;
+	}
+	int rnd = rand() % possible.size();
+	int yn = possible[rnd].first, xn = possible[rnd].second;
+	if (tablePl[yn][xn] == 'k') {
+		if (yn == y) {
+			u = d = 0;
+			if (xn == xl - 1)
+				xl--;
+			else
+				xr++;
+		}
+		else {
+			r = l = 0;
+			if (yn == yu - 1)
 				yu--;
-				tablePl[yu][x] = 'x';
-				tableBot[yu][x] = 'x';
-				l = r = 0;
-				return;
-			}
 			else
-				u = 0;
+				yd++;
 		}
-		else if (d && yd + 1 <= 10) {
-			xn = x; yn = ++yd;
-			if (tablePl[yd][x] == 'k')
-			{
-				tablePl[yd][x] = 'x';
-				tableBot[yd][x] = 'x';
-				l = r = 0;
-				return;
-			}
-			else
-				d = 0;
+		tablePl[yn][xn] = 'x';
+		tableBot[yn][xn] = 'x';
+		if (!isShipExist(tablePl)) {
+			shot = 0;
+			l = 0; r = 0; d = 0; u = 0;
 		}
-
 	}
 	else {
-		rnd = rand() % 2;
-		if (rnd && l && xl - 1 >= 0) {
-			xn = xl-1; yn = y;
-			if (tablePl[y][xl - 1] == 'k')
-			{
-				xl--;
-				tablePl[y][xl] = 'x';
-				tableBot[y][xl] = 'x';
-				u = d = 0;
-				return;
-			}
-			else
-				l = 0;
-		}
-		else if (d && xr + 1 <= 10) {
-			xn = xr+1; yn = y;
-			if (tablePl[y][xr + 1] == 'k')
-			{
-				xr++;
-				tablePl[y][xr] = 'x';
-				tableBot[y][xr] = 'x';
-				u = d = 0;
-				return;
-			}
-			else
-				r = 0;
-		}
+		if (xn == xl-1)
+			l = 0;
+		else if (xn == xr+1)
+			r = 0;
+		else if (yn == yu-1)
+			u = 0;
+		else if (yn == yd+1)
+			d = 0;
+		tablePl[yn][xn] = 'o';
+		tableBot[yn][xn] = 'o';
+
 	}
-	tableBot[yn][xn] = 'o';
-	tablePl[yn][xn] = 'o';
 }
 
 void aiRnd(char** tableBot, char** tablePl)
@@ -238,14 +223,15 @@ void aiRnd(char** tableBot, char** tablePl)
 		}
 	}
 	int rnd = rand() % free.size();
-	int y = free[rnd].first;
-	int x = free[rnd].second;
+	y = free[rnd].first;
+	x = free[rnd].second;
 	tableBot[y][x] = (tablePl[y][x] == 'k') ? 'x' : 'o';
+	tablePl[y][x] = (tablePl[y][x] == 'k') ? 'x' : 'o';
+	if (tableBot[y][x] == 'x')
+		shot = 1;
 }
 
 void aiHub() {
-	if (shot && !isShipExist(tableplship))
-		shot = 0;
 	if (shot)
 		aiIfShot(tablebotshoot, tableplship);
 	else
@@ -267,15 +253,16 @@ int main()
 	//preparing_field();
 	filling(tableplship);
 	filling(tablebotshoot);
-	tableplship[1][0] = 'k';
-	tableplship[1][1] = 'x';
-	tablebotshoot[1][1] = 'x';
-	tableplship[1][2] = 'k';
-	tableplship[1][3] = 'k';
-	y = 1; x = 1;
-	shot = 1;
-	aiIfShot(tablebotshoot, tableplship);
-	aiIfShot(tablebotshoot, tableplship);
+	tableplship[1][1+1] = 'k';
+	tableplship[1][4+1] = 'k';
+	tableplship[1][3+1] = 'k';
+	tableplship[1][2+1] = 'k';
+	do {
+		aiHub();
+	} while (!shot);
+	do {
+		aiHub();
+	} while (shot);
 	print(tableplship);
 	print(tablebotshoot);
 }
